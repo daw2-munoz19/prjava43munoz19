1f4528438c923d51d5eae5f3dc2594e909b7c560
u19 <u19@ubuNB-munoz19>
2018-11-07 16:36:54 +0100

    Versió 0.7-C del projecte prjava43munoz19. Branca master modificada

--- src/prjava43munoz19/Prjava43munoz19.java
+++ src/prjava43munoz19/Prjava43munoz19.java
@@ -1,6 +1,7 @@
 package prjava43munoz19;
 import java.awt.*; // using AWT containers and components
 import java.awt.event.*; // using AWT events and listener interfaces
+import java.sql.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class Prjava43munoz19 extends Frame
 implements ActionListener, WindowListener {
